0c5f1a18b84c35f4eb1d226cb65d6120b6de694d
Alejandro Lopez <alejo32267@gmail.com>
2025-07-02 12:40:26 -0500

    adelantando

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,71 @@
 import React from "react";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Footer from "./Footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const data = [
+  {
+    title: "Lorem ",
+    comentario: "Llorem ipsum",
+    img: lorem,
+    button: "Leer más",
+  },
+  {
+    title: "Lorem ",
+    comentario: "Llorem ipsum",
+    img: lorem,
+    button: "Leer más",
+  },
+  {
+    title: "Lorem ",
+    comentario: "Llorem ipsum",
+    img: lorem,
+    button: "Leer más",
+  },
+  {
+    title: "Lorem ",
+    comentario: "Llorem ipsum",
+    img: lorem,
+    button: "Leer más",
+  },
+];
+
+const Home = () => {
+  for (let item of data) {
+    const card = (
+      <div className=" col-lg-3 col-md-6">
+        <Card
+          title={item.title}
+          comentario={item.comentario}
+          img={item.img}
+          button={item.button}
+        />
+      </div>
+    );
+  }
+  return (
+    <>
+      <Navbar />
+      <div className="container" style={{ padddingTop: "20px" }}>
+        <Jumbotron />
+        <div
+          className="row"style={{ paddingBottom: "30px", paddingTop: "20px" }}
+        >
+          {info}
+        </div>
+      </div>
+	  <Footer/>
+    </>
+  );
 };
 
 export default Home;
